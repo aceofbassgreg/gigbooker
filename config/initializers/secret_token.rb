6fe0d81a87c33a43886c4b0aaa250b2339a26d9b
aceofbassgreg <gregory.kenenitz@gmail.com>
2013-05-16 22:01:18 -0400

    Initial commit - using ps, ruby 1.9.3-p429

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gigbooker::Application.config.secret_token = '49f64282a6a3073bd6942fe1dd08c5454982ade8662e8d9934a20843e7f96ede0d07a051b10d1b0651565ddfa242aea03282432605f223b3d762b98edfa8c0fb'
