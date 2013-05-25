a840edd31ff2a7406549e5f2697a6282d7149504
aceofbassgreg <gregory.kenenitz@gmail.com>
2013-05-25 08:32:31 -0400

    Specs for initial band model

--- spec/models/band_spec.rb
+++ spec/models/band_spec.rb
@@ -1,5 +1,44 @@
 require 'spec_helper'
 
 describe Band do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "basic attributes" do
+    before { @band = create(:band) }
+    subject { @band }
+
+    it { should be_valid }
+    it { should respond_to(:name) }
+    it { should respond_to(:contact_email) }
+    it { should respond_to(:travel_radius) }
+    it { should respond_to(:location) }
+  end 
+
+  describe "with invalid params" do
+    context "no name" do
+      before { @band = build(:band, name: " ") }
+      subject { @band }
+
+      it { should_not be_valid }
+    end
+
+    context "no contact email" do
+      before { @band = build(:band, contact_email: " ") }
+      subject { @band }
+
+      it { should_not be_valid }
+    end
+
+    context "no travel radius" do
+      before { @band = build(:band, travel_radius: " ") }
+      subject { @band }
+
+      it { should_not be_valid }
+    end
+
+    context "no location" do
+      before { @band = build(:band, location: " ") }
+      subject { @band }
+
+      it { should_not be_valid }
+    end
+  end
 end
