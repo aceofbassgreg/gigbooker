421db4ef77854f3f4e0fcf4078576f6d84f7d571
aceofbassgreg <gregory.kenenitz@gmail.com>
2013-05-25 09:48:19 -0400

    Creating new venue registration form & basic venue show page, passing new_venue feature specs

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -1,9 +1,22 @@
 class VenuesController < ApplicationController
   def new
-    
+    @venue = Venue.new
+    @address = @venue.build_address
+  end
+
+  def create
+    @venue = Venue.new(params[:venue])
+    @address = @venue.build_address(params[:address])
+    if @venue.save
+      flash[:success] = "Congratulations! Your venue has been added!"
+      redirect_to @venue
+    else
+      flash[:error] = "Please fill out all fields."
+      render 'new'
+    end
   end
 
   def show
-    
+    @venue = Venue.find(params[:id])
   end
 end
